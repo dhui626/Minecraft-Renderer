acae2bb449d56731792b2fcd252341c1ec6a3f9d
DESKTOP-D3OHIDL\dhui <linzhehui626@mail.ustc.edu.cn>
2024-10-02 17:31:05 +0800

    draw triangles

--- src/main.cpp
+++ src/main.cpp
@@ -27,20 +27,30 @@ int main(void)
 
     std::cout << "OpenGL Version:\t" << glGetString(GL_VERSION) << std::endl;
 
+    float positions[12] = {
+         -0.5f, 0.5f,
+          0.0f, 0.0f,
+         -0.0f, 0.2f,
+         -0.1f, 0.3f,
+         -0.2f, 0.5f,
+         -0.1f, 0.8f
+    };
+
+    unsigned int buffer;
+    glGenBuffers(1, &buffer);
+    glBindBuffer(GL_ARRAY_BUFFER, buffer);
+    glBufferData(GL_ARRAY_BUFFER, 12 * sizeof(float), positions, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2*sizeof(float), 0);
+    glEnableVertexAttribArray(0);
+
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
 
-        glBegin(GL_TRIANGLES);
-        glVertex2f(-0.5f, 0.5f);
-        glVertex2f(0.0f, 0.0f);
-        glVertex2f(-0.0f, 0.2f);
-        glVertex2f(-0.1f, 0.3f);
-        glVertex2f(-0.2f, 0.5f);
-        glVertex2f(-0.1f, 0.8f);
-        glEnd();
+        glDrawArrays(GL_TRIANGLES, 0, 6);
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
