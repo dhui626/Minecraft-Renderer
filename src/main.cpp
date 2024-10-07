acbab4469267908d74256bf85a0395b3bba29fed
DESKTOP-D3OHIDL\dhui <linzhehui626@mail.ustc.edu.cn>
2024-10-07 16:33:57 +0800

    add index buffer

--- src/main.cpp
+++ src/main.cpp
@@ -113,22 +113,36 @@ int main(void)
 
     std::cout << "OpenGL Version:\t" << glGetString(GL_VERSION) << std::endl;
 
-    float positions[12] = {
+    float positions[] = {
          -0.5f, 0.5f,
-          0.0f, 0.0f,
+          0.2f, -0.1f,
          -0.0f, 0.2f,
          -0.1f, 0.3f,
          -0.2f, 0.5f,
          -0.1f, 0.8f
     };
 
-    unsigned int buffer;
-    glGenBuffers(1, &buffer);
-    glBindBuffer(GL_ARRAY_BUFFER, buffer);
+    unsigned int indices[] = {
+         0,1,2,
+         0,3,4,
+         3,4,5,
+         1,2,5
+    };
+
+    // vertex buffer
+    unsigned int vbo;
+    glGenBuffers(1, &vbo);
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
     glBufferData(GL_ARRAY_BUFFER, 12 * sizeof(float), positions, GL_STATIC_DRAW);
 
     glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2*sizeof(float), 0);
     glEnableVertexAttribArray(0);
+
+    // index buffer
+    unsigned int ibo;
+    glGenBuffers(1, &ibo);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, 12 * sizeof(unsigned int), indices, GL_STATIC_DRAW);
 
     //Load shader file
     ShaderProgramSource source = ParseShader("res/shaders/Basic.shader");
@@ -145,7 +159,7 @@ int main(void)
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
 
-        glDrawArrays(GL_TRIANGLES, 0, 6);
+        glDrawElements(GL_TRIANGLES, 12, GL_UNSIGNED_INT, nullptr);
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
