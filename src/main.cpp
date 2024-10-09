7b9ee89c368e26dd50830e8c99b45809efc7c2ed
DESKTOP-D3OHIDL\dhui <linzhehui626@mail.ustc.edu.cn>
2024-10-09 18:54:18 +0800

    debug implemention

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,26 @@
 #include <string>
 #include <sstream>
 
+#define ASSERT(x) if(!(x)) __debugbreak();
+#define GLCall(x) GLClearError(); x; ASSERT(GLLogCall(#x, __FILE__, __LINE__));
+
+static void GLClearError()
+{
+    while (glGetError() != GL_NO_ERROR);    
+}
+
+static bool GLLogCall(const char* function, const char* file, int line)
+{
+    while (GLenum error = glGetError())
+    {
+        std::cout << "[OpenGL Error] (" << error << ")" 
+            << "\nFunction: " << function << "\nFile: " << file << "\nLine: " << line << std::endl;
+        return false;
+    }
+    return true;
+}
+
+
 struct ShaderProgramSource
 {
     std::string vertexSource;
@@ -159,7 +179,8 @@ int main(void)
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
 
-        glDrawElements(GL_TRIANGLES, 12, GL_UNSIGNED_INT, nullptr);
+        //glDrawElements(GL_TRIANGLES, 12, GL_UNSIGNED_INT, nullptr);
+        GLCall(glDrawElements(GL_TRIANGLES, 12, GL_INT, nullptr)); // wrong 
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
