448df4398fc98546cdce660e2696c7edb6650a26
DESKTOP-D3OHIDL\dhui <linzhehui626@mail.ustc.edu.cn>
2024-10-24 18:04:06 +0800

    moving camera

--- src/Renderer.cpp
+++ src/Renderer.cpp
@@ -28,6 +28,5 @@ void Renderer::Draw(const VertexArray& va, const IndexBuffer& ib, const Shader& 
     shader.Bind();
     va.Bind();
     ib.Bind();
-    glDrawArrays(GL_TRIANGLES, 0, 36);
-    //GLCall(glDrawElements(GL_TRIANGLES, ib.GetCount(), GL_UNSIGNED_INT, nullptr));
+    GLCall(glDrawElements(GL_TRIANGLES, ib.GetCount(), GL_UNSIGNED_INT, nullptr));
 }
--- src/main.cpp
+++ src/main.cpp
@@ -14,6 +14,7 @@
 #include "Shader.h"
 #include "Texture.h"
 
+
 int main(void)
 {
     GLFWwindow* window;
@@ -44,61 +45,30 @@ int main(void)
 
     std::cout << "OpenGL Version:\t" << glGetString(GL_VERSION) << std::endl;
 
-    //float positions[] = {
-    //     //position       //texture coord  //color
-    //     -0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, //0
-    //      0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f, 1.0f, 0.0f, //1
-    //      0.5f, 0.5f,  0.0f, 1.0f, 1.0f, 0.0f, 0.0f, 1.0f, //2
-    //     -0.5f, 0.5f,  0.0f, 0.0f, 1.0f, 1.0f, 0.0f, 1.0f, //3
-    //};
-
     float positions[] = {
-    -0.5f, -0.5f, -0.5f,  0.0f, 0.0f, 0.2f, 0.3f, 0.8f,
-     0.5f, -0.5f, -0.5f,  1.0f, 0.0f, 0.2f, 0.3f, 0.8f,
-     0.5f,  0.5f, -0.5f,  1.0f, 1.0f, 0.2f, 0.3f, 0.8f,
-     0.5f,  0.5f, -0.5f,  1.0f, 1.0f, 0.2f, 0.3f, 0.8f,
-    -0.5f,  0.5f, -0.5f,  0.0f, 1.0f, 0.2f, 0.3f, 0.8f,
-    -0.5f, -0.5f, -0.5f,  0.0f, 0.0f, 0.2f, 0.3f, 0.8f,
-
-    -0.5f, -0.5f,  0.5f,  0.0f, 0.0f, 0.2f, 0.3f, 0.8f,
-     0.5f, -0.5f,  0.5f,  1.0f, 0.0f, 0.2f, 0.3f, 0.8f,
-     0.5f,  0.5f,  0.5f,  1.0f, 1.0f, 0.2f, 0.3f, 0.8f,
-     0.5f,  0.5f,  0.5f,  1.0f, 1.0f, 0.2f, 0.3f, 0.8f,
-    -0.5f,  0.5f,  0.5f,  0.0f, 1.0f, 0.2f, 0.3f, 0.8f,
-    -0.5f, -0.5f,  0.5f,  0.0f, 0.0f, 0.2f, 0.3f, 0.8f,
-
-    -0.5f,  0.5f,  0.5f,  1.0f, 0.0f, 0.2f, 0.3f, 0.8f,
-    -0.5f,  0.5f, -0.5f,  1.0f, 1.0f, 0.2f, 0.3f, 0.8f,
-    -0.5f, -0.5f, -0.5f,  0.0f, 1.0f, 0.2f, 0.3f, 0.8f,
-    -0.5f, -0.5f, -0.5f,  0.0f, 1.0f, 0.2f, 0.3f, 0.8f,
-    -0.5f, -0.5f,  0.5f,  0.0f, 0.0f, 0.2f, 0.3f, 0.8f,
-    -0.5f,  0.5f,  0.5f,  1.0f, 0.0f, 0.2f, 0.3f, 0.8f,
-
-     0.5f,  0.5f,  0.5f,  1.0f, 0.0f, 0.2f, 0.3f, 0.8f,
-     0.5f,  0.5f, -0.5f,  1.0f, 1.0f, 0.2f, 0.3f, 0.8f,
-     0.5f, -0.5f, -0.5f,  0.0f, 1.0f, 0.2f, 0.3f, 0.8f,
-     0.5f, -0.5f, -0.5f,  0.0f, 1.0f, 0.2f, 0.3f, 0.8f,
-     0.5f, -0.5f,  0.5f,  0.0f, 0.0f, 0.2f, 0.3f, 0.8f,
-     0.5f,  0.5f,  0.5f,  1.0f, 0.0f, 0.2f, 0.3f, 0.8f,
-
-    -0.5f, -0.5f, -0.5f,  0.0f, 1.0f, 0.2f, 0.3f, 0.8f,
-     0.5f, -0.5f, -0.5f,  1.0f, 1.0f, 0.2f, 0.3f, 0.8f,
-     0.5f, -0.5f,  0.5f,  1.0f, 0.0f, 0.2f, 0.3f, 0.8f,
-     0.5f, -0.5f,  0.5f,  1.0f, 0.0f, 0.2f, 0.3f, 0.8f,
-    -0.5f, -0.5f,  0.5f,  0.0f, 0.0f, 0.2f, 0.3f, 0.8f,
-    -0.5f, -0.5f, -0.5f,  0.0f, 1.0f, 0.2f, 0.3f, 0.8f,
-
-    -0.5f,  0.5f, -0.5f,  0.0f, 1.0f, 0.2f, 0.3f, 0.8f,
-     0.5f,  0.5f, -0.5f,  1.0f, 1.0f, 0.2f, 0.3f, 0.8f,
-     0.5f,  0.5f,  0.5f,  1.0f, 0.0f, 0.2f, 0.3f, 0.8f,
-     0.5f,  0.5f,  0.5f,  1.0f, 0.0f, 0.2f, 0.3f, 0.8f,
-    -0.5f,  0.5f,  0.5f,  0.0f, 0.0f, 0.2f, 0.3f, 0.8f,
-    -0.5f,  0.5f, -0.5f,  0.0f, 1.0f, 0.2f, 0.3f, 0.8f
+        -0.5f, -0.5f,  0.5f,  0.0f, 0.0f, 0.2f, 0.3f, 0.8f, // 0
+         0.5f, -0.5f,  0.5f,  1.0f, 0.0f, 0.2f, 0.3f, 0.8f, // 1
+         0.5f,  0.5f,  0.5f,  1.0f, 1.0f, 0.2f, 0.3f, 0.8f, // 2
+        -0.5f,  0.5f,  0.5f,  0.0f, 1.0f, 0.2f, 0.3f, 0.8f, // 3
+        -0.5f, -0.5f, -0.5f,  0.0f, 0.0f, 0.2f, 0.3f, 0.8f, // 4
+         0.5f, -0.5f, -0.5f,  1.0f, 0.0f, 0.2f, 0.3f, 0.8f, // 5
+         0.5f,  0.5f, -0.5f,  1.0f, 1.0f, 0.2f, 0.3f, 0.8f, // 6
+        -0.5f,  0.5f, -0.5f,  0.0f, 1.0f, 0.2f, 0.3f, 0.8f  // 7
     };
 
     unsigned int indices[] = {
-         0,1,2,
-         0,2,3
+        0, 1, 2,
+        2, 3, 0,
+        4, 5, 6,
+        6, 7, 4,
+        3, 2, 6,
+        6, 7, 3,
+        0, 4, 5,
+        5, 1, 0,
+        3, 7, 6,
+        6, 2, 3,
+        0, 1, 5,
+        5, 4, 0
     };
 
     GLCall(glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA));
@@ -131,15 +101,21 @@ int main(void)
     shader.SetUniformMat4f("u_Model", model);
 
     glm::mat4 view(1.0f);
-    view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
+    glm::vec3 cameraPos = glm::vec3(0.0f, 0.0f, 3.0f);
+    glm::vec3 cameraTarget = glm::vec3(0.0f, 0.0f, 0.0f);
+    glm::vec3 cameraDirection = glm::normalize(cameraPos - cameraTarget);
+    glm::vec3 up = glm::vec3(0.0f, 1.0f, 0.0f);
+    glm::vec3 cameraRight = glm::normalize(glm::cross(up, cameraDirection));
+    glm::vec3 cameraFront = glm::vec3(0.0f, 0.0f, -1.0f);
+    glm::vec3 cameraUp = glm::vec3(0.0f, 1.0f, 0.0f);
+    view = glm::lookAt(cameraPos, cameraTarget, up);
     shader.SetUniformMat4f("u_View", view);
 
     //glm::mat4 proj2 = glm::ortho(-2.0f, 2.0f, -1.5f, 1.5f, -1.0f, 1.0f);
     glm::mat4 proj = glm::perspective(glm::radians(45.0f), 4.0f / 3.0f, 0.1f, 100.0f);
     shader.SetUniformMat4f("u_Proj", proj);
 
-    glm::vec4 temp = proj * view * model * glm::vec4(0.5f, 0.5f, -1, 1);
-    temp /= temp.a;
+
 
     Texture texture("res/textures/ChernoLogo2.png");
     texture.Bind();
@@ -166,13 +142,27 @@ int main(void)
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
+        /* Press Input */
+        float cameraSpeed = 0.05f; // adjust accordingly
+        if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+            cameraPos += cameraSpeed * cameraFront;
+        if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+            cameraPos -= cameraSpeed * cameraFront;
+        if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+            cameraPos -= cameraSpeed * cameraRight;
+        if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+            cameraPos += cameraSpeed * cameraRight;
+
         /* Render here */
         renderer.Clear();
 
         shader.Bind();
         shader.SetUniform4f("u_Color", red, green, 0.8f, 1.0f);
 
-        model = glm::rotate(model, (float)glfwGetTime() * glm::radians(50.0f), glm::vec3(0.5f, 1.0f, 0.0f));
+        view = glm::lookAt(cameraPos, cameraPos + cameraFront, up);
+        shader.SetUniformMat4f("u_View", view);
+
+        model = glm::rotate(model, (float)glfwGetTime() * glm::radians(0.2f), glm::vec3(0.5f, 1.0f, 0.0f));
         shader.SetUniformMat4f("u_Model", model);
 
         renderer.Draw(va, ib, shader); 
@@ -187,8 +177,8 @@ int main(void)
         else if (green < 0.0f)
             increment2 = 0.02f;
 
-        red += increment;
-        green += increment2;
+        //red += increment;
+        //green += increment2;
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
