e7adec963996d7d67cc38959078f988b6f1fa267
DESKTOP-D3OHIDL\dhui <linzhehui626@mail.ustc.edu.cn>
2024-10-26 17:50:49 +0800

    add imgui

--- src/main.cpp
+++ src/main.cpp
@@ -2,9 +2,14 @@
 #include <GLFW/glfw3.h>
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
 
 #include <string>
 #include <iostream>
+
+#include "imgui.h"
+#include "imgui_impl_glfw.h"
+#include "imgui_impl_opengl3.h"
 
 #include "Renderer.h"
 #include "VertexBuffer.h"
@@ -47,6 +52,12 @@ int main(void)
         return -1;
 
     std::cout << "OpenGL Version:\t" << glGetString(GL_VERSION) << std::endl;
+
+    /* ImGui Setup */
+    ImGui::CreateContext();
+    ImGui::StyleColorsDark();
+    ImGui_ImplGlfw_InitForOpenGL(window, true);
+    ImGui_ImplOpenGL3_Init("#version 330 core");
 
     float positions[] = {
         // Vertex          // Normal           // Coords
@@ -106,101 +117,119 @@ int main(void)
     //GLCall(glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA));
     //GLCall(glEnable(GL_BLEND));
 
-    // vertex array
-    VertexArray va;
-    va.Bind();
-
-    // vertex buffer
-    VertexBuffer vb(positions, sizeof(positions));
-
-    // index buffer
-    IndexBuffer ib(indices, sizeof(indices) / sizeof(unsigned int));
-
-    VertexBufferLayout layout;
-    layout.Push<float>(3);
-    layout.Push<float>(3);
-    layout.Push<float>(2);
-    va.AddBuffer(vb, layout);
-
-    //Load shader file
-    Shader shader("res/shaders/Basic.shader");
-    shader.Bind();
-
-    // Camera 
-    Camera camera(45.0f, 0.1f, 100.0f, window);
-    camera.OnResize(width, height);
-
-    //MVP matrix
-    glm::mat4 model(1.0f);
-    //model = glm::rotate(model, glm::radians(-55.0f), glm::vec3(1.0f, 0.0f, 0.0f));
-    shader.SetUniformMat4f("u_Model", model);
-
-    glm::mat4 view = camera.GetView();
-    shader.SetUniformMat4f("u_View", view);
-
-    //glm::mat4 proj2 = glm::ortho(-2.0f, 2.0f, -1.5f, 1.5f, -1.0f, 1.0f);
-    glm::mat4 proj = camera.GetProjection();
-    shader.SetUniformMat4f("u_Proj", proj);
-    
-    // Uniforms
-    glm::vec3 lightPos{ 2.0f,3.0f,1.0f };
-    shader.SetUniform3f("u_LightPos", lightPos);
-    float lightIntensity = 10.0f;
-    shader.SetUniform1f("u_LightIntensity", lightIntensity);
-    shader.SetUniform3f("u_CameraPos", camera.GetPosition());
-    float Kd = 1.0f; //diffuse K
-    shader.SetUniform1f("u_Kd", Kd);
-    float Ks = 1.0f; //specular K
-    shader.SetUniform1f("u_Ks", Ks);
-
-    // Texture
-    Texture texture("res/textures/1.20.2_minecraft_textures_atlas_blocks_0.png");
-    texture.Bind();
-    shader.SetUniform1i("u_Texture", 0);
-
-    // Unbind
-    shader.Unbind();
-    va.Unbind();
-    vb.Unbind();
-    ib.Unbind();
-
-    Renderer renderer;
-
-    glEnable(GL_DEPTH_TEST);
-
-    /* Loop until the user closes the window */
-    while (!glfwWindowShouldClose(window))
     {
-        /* Press Input */
-        camera.OnUpdate(1.0f / 30.0f); // 30hz
+        // vertex array
+        VertexArray va;
+        va.Bind();
 
-        /* Render here */
-        renderer.Clear();
+        // vertex buffer
+        VertexBuffer vb(positions, sizeof(positions));
 
+        // index buffer
+        IndexBuffer ib(indices, sizeof(indices) / sizeof(unsigned int));
+
+        VertexBufferLayout layout;
+        layout.Push<float>(3);
+        layout.Push<float>(3);
+        layout.Push<float>(2);
+        va.AddBuffer(vb, layout);
+
+        //Load shader file
+        Shader shader("res/shaders/Basic.shader");
         shader.Bind();
 
-        //Uniforms
-        model = glm::rotate(model, (float)glfwGetTime() * glm::radians(0.1f), glm::vec3(0.5f, 1.0f, 0.0f));
+        // Camera 
+        Camera camera(45.0f, 0.1f, 100.0f, window);
+        camera.OnResize(width, height);
+
+        //MVP matrix
+        glm::mat4 model(1.0f);
+        glm::vec3 translation{ 1.0f, 1.0f, 1.0f };
+        model = glm::translate(model, translation);
+        //model = glm::rotate(model, glm::radians(-55.0f), glm::vec3(1.0f, 0.0f, 0.0f));
         shader.SetUniformMat4f("u_Model", model);
 
-        view = camera.GetView();
+        glm::mat4 view = camera.GetView();
         shader.SetUniformMat4f("u_View", view);
 
-        proj = camera.GetProjection();
+        //glm::mat4 proj2 = glm::ortho(-2.0f, 2.0f, -1.5f, 1.5f, -1.0f, 1.0f);
+        glm::mat4 proj = camera.GetProjection();
         shader.SetUniformMat4f("u_Proj", proj);
 
+        // Uniforms
+        glm::vec3 lightPos{ 2.0f,3.0f,1.0f };
+        shader.SetUniform3f("u_LightPos", lightPos);
+        float lightIntensity = 10.0f;
+        shader.SetUniform1f("u_LightIntensity", lightIntensity);
         shader.SetUniform3f("u_CameraPos", camera.GetPosition());
+        float Kd = 1.0f; //diffuse K
+        shader.SetUniform1f("u_Kd", Kd);
+        float Ks = 1.0f; //specular K
+        shader.SetUniform1f("u_Ks", Ks);
 
-        renderer.Draw(va, ib, shader); 
+        // Texture
+        Texture texture("res/textures/1.20.2_minecraft_textures_atlas_blocks_0.png");
+        texture.Bind();
+        shader.SetUniform1i("u_Texture", 0);
 
-        /* Swap front and back buffers */
-        glfwSwapBuffers(window);
+        // Unbind
+        shader.Unbind();
+        va.Unbind();
+        vb.Unbind();
+        ib.Unbind();
 
-        /* Poll for and process events */
-        glfwPollEvents();
+        Renderer renderer;
+
+        glEnable(GL_DEPTH_TEST);
+
+        /* Loop until the user closes the window */
+        while (!glfwWindowShouldClose(window))
+        {
+            /* Press Input */
+            camera.OnUpdate(1.0f / 30.0f); // 30hz
+
+            /* Render here */
+            renderer.Clear();
+
+            shader.Bind();
+
+            //Uniforms
+            model = glm::translate(glm::mat4{ 1.0 }, translation);
+            shader.SetUniformMat4f("u_Model", model);
+
+            view = camera.GetView();
+            shader.SetUniformMat4f("u_View", view);
+
+            proj = camera.GetProjection();
+            shader.SetUniformMat4f("u_Proj", proj);
+
+            shader.SetUniform3f("u_CameraPos", camera.GetPosition());
+
+            renderer.Draw(va, ib, shader);
+
+            ImGui_ImplOpenGL3_NewFrame();
+            ImGui_ImplGlfw_NewFrame();
+            ImGui::NewFrame();
+            {
+                ImGui::Begin("Model Configs");
+                ImGui::DragFloat3("Model Position", glm::value_ptr(translation), 0.1f);
+                ImGui::Text("This is some useful text.");
+                ImGui::End();
+            }
+            ImGui::Render();
+            ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+            /* Swap front and back buffers */
+            glfwSwapBuffers(window);
+
+            /* Poll for and process events */
+            glfwPollEvents();
+        }
+        ImGui_ImplOpenGL3_Shutdown();
+        ImGui_ImplGlfw_Shutdown();
+        ImGui::DestroyContext();
+    // Destructor need to be called AFTER vbo/ibo is deleted (fixed)
     }
-
-    // Need to be called AFTER vbo/ibo is deleted (unfixed)
     glfwTerminate();
     return 0;
 }
